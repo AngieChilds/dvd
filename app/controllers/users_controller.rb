70f1eb9b9da5debea560a57bbd9fb206486df13c
AngieChilds <pirateprincess_avh@yahoo.com>
2017-04-16 01:48:06 +0200

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      flash.now[:success] = "Welcime to DVD!"
+      log_in @user
+      flash[:success] = "Welcome to DVD!"
       redirect_to @user
     else
       render 'new'
